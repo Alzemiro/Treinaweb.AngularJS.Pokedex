cfae54c4cbfc7304902da95aca1acca204669cb9
Iago Moraes <iago.moraes@syonet.com>
2020-09-14 14:39:04 -0300

    First commit

---
+++ js/components/PokeListItem/PokeListItemComponent.js
@@ -0,0 +1,13 @@
+angular.module('pokedexApp')
+.component('pokeListItemComponent', {
+    templateUrl:'./js/components/PokeListItem/PokeListItemComponent.html',
+    controller: PokeListItemController,
+    bindings: {
+        pkm: '='
+    }
+
+});
+
+function PokeListItemController(){
+    
+}
